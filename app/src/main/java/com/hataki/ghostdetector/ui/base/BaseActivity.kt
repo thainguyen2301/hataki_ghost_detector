d13cf499e8bd632ba67ff51d07a4e7c9fa7d6ec9
Lê Quang Đạo <85027828+lequangdao0303@users.noreply.github.com>
2025-10-23 02:00:37 +0700

    Reskin Add custom text Update UI Splash, Language, Intro,

--- app/src/main/java/com/hataki/ghostdetector/ui/base/BaseActivity.kt
+++ app/src/main/java/com/hataki/ghostdetector/ui/base/BaseActivity.kt
@@ -4,7 +4,11 @@ import android.content.Context
 import android.os.Bundle
 import android.view.WindowManager
 import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
@@ -22,14 +26,44 @@ abstract class BaseActivity<VM : BaseViewModel, VB : ViewBinding> : AppCompatAct
     protected val viewModel: VM by lazy {
         ViewModelProvider(this)[viewModelClass()]
     }
-
+    protected open val belowStatusBar = true
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, getLayoutResource())
         window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
         setContentView(binding.root)
+        hideNavigationBar()
+        if (belowStatusBar) {
+            drawBelowStatusBar()
+        }
         onCreateImpl()
         setupObservers()
+    }
+
+    private fun hideNavigationBar() {
+        WindowInsetsControllerCompat(window, binding.root).let { controller ->
+            controller.hide(WindowInsetsCompat.Type.navigationBars())
+            controller.systemBarsBehavior =
+                WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            controller.isAppearanceLightStatusBars = false
+        }
+    }
+
+    private fun drawBelowStatusBar() {
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+            val imeVisible = insets.isVisible(WindowInsetsCompat.Type.ime())
+            val imeInsets = insets.getInsets(WindowInsetsCompat.Type.ime())
+            val statusBarsInsets = insets.getInsets(WindowInsetsCompat.Type.statusBars())
+            val insetBottom = if (imeVisible) imeInsets.bottom else statusBarsInsets.bottom
+            v.setPadding(
+                statusBarsInsets.left,
+                statusBarsInsets.top,
+                statusBarsInsets.right,
+                insetBottom
+            )
+            insets
+        }
     }
 
     override fun attachBaseContext(newBase: Context?) {
