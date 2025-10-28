4836b39cc8e210d68c7a8ed2ad1950cb93d53777
ThaiNguyen <thainx23@gmail.com>
2025-10-28 17:09:43 +0700

    add some code

--- app/src/main/java/com/ghostfinder/ghostdetector/radar/ui/common/FeedbackBottomSheet.kt
+++ app/src/main/java/com/ghostfinder/ghostdetector/radar/ui/common/FeedbackBottomSheet.kt
@@ -1,10 +1,13 @@
 package com.ghostfinder.ghostdetector.radar.ui.common
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.content.FileProvider
+import com.ghostfinder.ghostdetector.radar.ads.AppAdvertiseManager
 import com.ghostfinder.ghostdetector.radar.databinding.LayoutBottomSheetFeedbackBinding
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
@@ -28,9 +31,32 @@ class FeedbackBottomSheet : BottomSheetDialogFragment() {
         binding.btnSend.setOnClickListener {
             val subject = binding.edtSubject.text.toString()
             val feedback = binding.edtFeedback.text.toString()
-            Toast.makeText(requireContext(), "Send: $subject - $feedback", Toast.LENGTH_SHORT)
-                .show()
-            dismiss()
+            if (subject.isNotEmpty() || feedback.isNotEmpty()) {
+                val emailIntent = Intent(Intent.ACTION_SEND).apply {
+                    type = "text/plain"
+                    putExtra(Intent.EXTRA_EMAIL, arrayOf("hataki.support@gmail.com"))
+                    putExtra(Intent.EXTRA_SUBJECT, subject)
+                    putExtra(Intent.EXTRA_TEXT, feedback)
+                    val logFile = AppAdvertiseManager.fileLogger.getLogFile(requireContext())
+                    val fileUri = FileProvider.getUriForFile(
+                        requireContext(),
+                        "${requireContext().packageName}.fileprovider",  // Replace with your app's authority
+                        logFile
+                    )
+                    putExtra(Intent.EXTRA_STREAM, fileUri)
+                    addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                }
+                emailIntent.setPackage("com.google.android.gm")
+                try {
+                    startActivity(emailIntent)
+                } catch (e: Exception) {
+                    Toast.makeText(
+                        requireContext(),
+                        "Gmail app is not installed",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
         }
         binding.btnClose.setOnClickListener {
             dismiss()
