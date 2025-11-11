855e95fc6003a1c7f30235e06266080f4dfcc817
ThaiNguyen <thainx23@gmail.com>
2025-11-11 20:53:14 +0800

    remove ad

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -93,5 +93,5 @@ dependencies {
     implementation(libs.androidx.datastore.preferences)
     implementation(libs.review.ktx)
     implementation(libs.androidx.preference.ktx)
-    implementation("com.google.android.gms:play-services-ads:24.7.0")
+//    implementation("com.google.android.gms:play-services-ads:24.7.0")
 }
